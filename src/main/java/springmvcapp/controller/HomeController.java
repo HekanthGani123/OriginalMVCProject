65d5547004c3660c6fb6d21794eb5c508ee870d5
hekanth <hekanth5597@gmail.com>
2023-04-30 12:46:42 +0530

    mvc working

---
+++ src/main/java/springmvcapp/controller/HomeController.java
@@ -0,0 +1,15 @@
+package springmvcapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/home")
+	public String home() {
+		System.out.println("This is a home controller");
+		return "index";
+	}
+
+}
